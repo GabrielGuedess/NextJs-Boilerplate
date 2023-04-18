eb886fc853d9caf844f07d73648ab8db195c23d1
Gabriel Guedes <gabrielrguedess@gmail.com>
2023-04-18 06:06:42 -0300

    test: logo props covered

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     'src/**/*.ts(x)?',
     '!src/app/**/*.tsx',
     '!src/pages/**/*.tsx',
+    '!src/stories/**/*.ts(x)?',
     '!src/**/stories.tsx',
     '!src/styles/**/*.ts',
     '!src/utils/tests/*.ts(x)?',
--- src/components/atoms/Logo/test.tsx
+++ src/components/atoms/Logo/test.tsx
@@ -1,11 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { Logo } from '.';
 
 describe('<Logo />', () => {
   it('should render the logo correctly', () => {
+    // Arrange
     const { container } = render(<Logo />);
 
+    // Assert
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render the logo without shadow', () => {
+    render(<Logo hasShadow={false} />);
+
+    // Assert
+    expect(screen.getByLabelText(/logo react/i)).toHaveStyleRule(
+      'display',
+      'none',
+      {
+        modifier: '&::after',
+      },
+    );
   });
 });
